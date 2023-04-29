ae8c8f62e9843fca6eee6eca2fdc3786192f1065
silje <angelviksilje@gmail.com>
2023-04-29 16:25:11 +0200

    fixed up some errors

--- src/utilities/handleLogout.js
+++ src/utilities/handleLogout.js
@@ -1,14 +1,30 @@
-export const handleLogout = ({userLogout}) => {
-    if (localStorage.getItem('accessToken')) {
-        console.log("You're logged in");
-        localStorage.removeItem('accessToken');
-        localStorage.removeItem('id');
-        localStorage.removeItem('name');
-        localStorage.removeItem('email');
-        localStorage.removeItem('avatar');
-        localStorage.removeItem('manager');
-    } else {
-        console.log("You're not logged in");
+import {Button} from 'antd';
+
+const HandleLogout = () => {
+
+    const logout = () => {
+        if (localStorage.getItem("accessToken")) {
+            console.log("You're logged in");
+            localStorage.removeItem('accessToken');
+            localStorage.removeItem('id');
+            localStorage.removeItem('name');
+            localStorage.removeItem('email');
+            localStorage.removeItem('avatar');
+            localStorage.removeItem('manager');
+            window.location.replace("/login");
+        }
+        console.log("You logged out.")
     }
-    userLogout();
-};
+
+    return (
+        <Button
+            onClick={logout}
+            color={"danger"}
+            type="primary"
+        >
+            Log out
+        </Button>
+    );
+}
+
+export default HandleLogout;
