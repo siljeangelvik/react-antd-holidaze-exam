44f02cb3cb29867f652ce3b0d91dd4882dd314a6
silje <angelviksilje@gmail.com>
2023-05-11 18:10:36 +0200

    "fixed" the login issue... * not finished

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,7 @@
 import {Content} from 'antd/es/layout/layout';
 import Title from 'antd/es/typography/Title';
 import React from 'react';
+import { Button, Result } from 'antd';
 
 function NotFound() {
     return (
@@ -10,6 +11,13 @@ function NotFound() {
                     <Title level={1}>404</Title>
                     <Title level={4}>Oops, sorry...Page not found!</Title>
                 </Content>
+
+                <Result
+                    status="404"
+                    title="404"
+                    subTitle="Sorry, the page you visited does not exist."
+                    extra={<Button type="primary">Back Home</Button>}
+                />
             </div>
         </>
     );
